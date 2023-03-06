b2e333facfbf890f8fa275ab737f2da61b2d11cf
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-03-07 00:43:31 +0200

    Трохи підправив маску номеру у скрипті

--- src/js/phone-mask.js
+++ src/js/phone-mask.js
@@ -1,4 +1,4 @@
 const selector = document.querySelector('.book-form-field[type="tel"]');
 console.log(selector);
-const im = new Inputmask("+38(099) 999-99-99");
+const im = new Inputmask("+ 38 (099) 999-99-99");
 im.mask(selector);
