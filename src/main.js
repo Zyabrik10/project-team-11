27b73339fbdaa6c5c48ef7da65376017ae3f8b70
Zyabrik10 <sasamazurok@gmail.com>
2023-03-06 14:33:18 +0300

    Slider bug fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+import './js/slick.min.js';
+import './js/slider-slick.js';
 import './js/modal.js';
 import './js/phone-mask.js';
 import './js/mobile-menu.js';
-import './js/slick.min.js';
-import './js/slider-slick.js';
