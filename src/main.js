9ad5a7acc3f379e4d02965e7dcdeda01950865a8
Zyabrik10 <sasamazurok@gmail.com>
2023-03-08 02:10:04 +0300

    Закоментував на час бібл. для  анімації

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import '/js/removeAnimation.js';
-import '/js/anchor.js';
+// import '/js/removeAnimation.js';
+// import '/js/anchor.js';
 import '/js/modal.js';
 import '/js/phone-mask.js';
 import '/js/mobile-menu.js';
