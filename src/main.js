9a16c3e335774460c4191f021348f7ff7c4a4761
Zyabrik10 <sasamazurok@gmail.com>
2023-03-08 16:45:56 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import '/js/animation/fm.revealator.jquery.js';
 import '/js/animation/jquery-1.11.3.min';
-import '/js/removeAnimation.js';
-import '/js/anchor.js';
+// import '/js/removeAnimation.js';
+// import '/js/anchor.js';
 import '/js/modal.js';
 import '/js/phone-mask.js';
 import '/js/mobile-menu.js';
