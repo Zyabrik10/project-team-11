19f15de281c9a078fb1e2ae4c4d558bb0a62d196
Zyabrik10 <sasamazurok@gmail.com>
2023-03-12 14:39:10 +0300

    Перепідключив скрипти для слайдеру

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import '/js/phone-mask.js';
 import '/js/animation/fm.revealator.jquery.js';
-import '/js/animation/jquery-1.11.3.min';
+// import '/js/animation/jquery-1.11.3.min.js';
+import '/js/phone-mask.js';
 import '/js/removeAnimation.js';
 import '/js/anchor.js';
 import '/js/modal-windows.js';
 import '/js/mobile-menu.js';
+import '/js/slider-slick.js';
