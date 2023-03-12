62619aa6bebb6614a012c970e007ca742b6b34c7
Zyabrik10 <sasamazurok@gmail.com>
2023-03-12 13:06:34 +0300

    Добавив слайдер, зробив усюди маску

--- src/js/phone-mask.js
+++ src/js/phone-mask.js
@@ -1,4 +1,7 @@
-const selector = document.querySelector('.book-form-field[type="tel"]');
-console.log(selector);
+const inputNumber = document.querySelectorAll('.book-form-field[type="tel"]');
 const im = new Inputmask("+ 38 (099) 999-99-99");
-im.mask(selector);
+
+inputNumber.forEach((selector) => {
+    im.mask(selector);
+});
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import '/js/phone-mask.js';
 import '/js/animation/fm.revealator.jquery.js';
 import '/js/animation/jquery-1.11.3.min';
+import '/js/slick.min.js';
+import '/js/slider-slick';
 import './js/removeAnimation.js';
-import '/js/modal.js';
-import '/js/mobile-menu.js';
+// import '/js/modal.js';
+// import '/js/mobile-menu.js';
 import '/js/anchor.js';
